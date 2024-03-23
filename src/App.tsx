34dd838ff13e59e07972b20fad29e69297c2c5c7
Piriya <piriyalapa@gmail.com>
2024-03-23 13:07:35 +0700

    update and deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import GlobalStyles from "./styles/GlobalStyles";
-import ScreenSizeDetect from "./components/screensizedetect/ScreenSizeDetect";
 import LandingPage from "./pages/LandingPage";
 
 export default class App extends React.Component {
@@ -8,10 +7,7 @@ export default class App extends React.Component {
     return (
       <div>
         <GlobalStyles />
-        <h1>Pod-request-access-landing-page</h1>
         <LandingPage />
-        <ScreenSizeDetect windowHeight={676} windowWidth={1820} screenNow="Mobile"/>
-        
       </div>
     );
   }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { StyledLandingPage } from "../styles/styles";
 import Header from "./sections/Header";
+import ScreenSizeDetectComponent from "../components/screensizedetect/ScreenSizeDetect";
+import { ScreenSizeDetect } from "../helpers/detectScreenSize";
 
 export default class LandingPage extends React.Component {
   // Define initial state to hold window dimensions
@@ -28,9 +30,17 @@ export default class LandingPage extends React.Component {
   }
 
   public render(): React.ReactNode {
+    const { windowWidth, windowHeight } = this.state;
+    const labelScreenNow = new ScreenSizeDetect(windowWidth);
+
     return (
       <StyledLandingPage>
         <Header />
+        <ScreenSizeDetectComponent
+          screenNow={labelScreenNow.detectScreenSize()}
+          windowWidth={windowWidth}
+          windowHeight={windowHeight}
+        />
       </StyledLandingPage>
     );
   }
